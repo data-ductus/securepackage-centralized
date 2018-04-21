424caaeb7b487669b78740e7dbb32ce8679d0535
maxkha-3 <maxkha-3@student.ltu.se>
2018-04-22 01:19:28 +0200

    Code clean-up and documentation of AgreementComponent

--- src/app/agreement/agreement.component.ts
+++ src/app/agreement/agreement.component.ts
@@ -1,32 +1,26 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {GenerationService} from '../services/generation.service';
 import {ApiService} from '../services/api.service';
 import {GlobalvarsService} from '../services/globalvars.service';
 import {AmChartsService, AmChart} from '@amcharts/amcharts3-angular';
 
-@Component({
-  selector: 'app-agreement',
-  templateUrl: './agreement.component.html',
-  styleUrls: ['./agreement.component.css']
-})
+/* AgreementComponent: handles agreement information display in detail for the seller and the buyer */
+@Component({selector: 'app-agreement', templateUrl: './agreement.component.html'})
+
 export class AgreementComponent implements OnInit {
   agreement_id;
 
-  stepDuration = 120;
+  current_location = {lat: null, lng: null};
 
-  current_location = {
-    lat: 24.799448,
-    lng: 120.979021,
-  };
-
+  //Agreement parameters
   item_agreement_params;
   item_item_params;
   item_terms;
-
   logistics_params;
   agreement_events;
 
+  //Charts
   private tempChart: AmChart;
   private pressChart: AmChart;
   private accChart: AmChart;
@@ -37,15 +31,18 @@ export class AgreementComponent implements OnInit {
   accData = [];
   pressData = [];
   humidityData = [];
-  currentPosition;
+
+  //Either return or delivery
   direction;
 
+  //Sensor inclusion
   accelerometer_sensor_id = null;
   pressure_sensor_id = null;
   temperature_sensor_id = null;
   humidity_sensor_id = null;
   gps_sensor_id = null;
 
+  //Thresholds
   sensor_pressure_low = null; sensor_pressure_high = null;
   sensor_humidity_low = null; sensor_humidity_high = null;
   sensor_temperature_low = null; sensor_temperature_high = null;
@@ -64,6 +61,7 @@ export class AgreementComponent implements OnInit {
     });
   }
 
+  //Initializes charts
   private initCharts = function() {
     this.tempChart = this.amCharts.makeChart( 'tempChartDiv', {
       'type': 'serial',
@@ -103,23 +101,31 @@ export class AgreementComponent implements OnInit {
     } );
   };
 
+  //Fetches agreement details and sets necessary variables
   fetchAgreementDetails = function () {
     let request_payload = {id: this.agreement_id, status: "ACCEPTED"};
     this.api.serverRequest(request_payload, "FETCH_AGREEMENT_INFO").then(response => {
       this.item_agreement_params = response;
+
+      //Determine the direction of package
       if (this.item_agreement_params.state == "TRANSIT" || this.item_agreement_params.state == "DELIVERED") {
         this.direction = "TRANSFER";
       }
       else if (this.item_agreement_params.state == "RETURN" || this.item_agreement_params.state == "RETURNED") {
         this.direction = "RETURN";
       }
+
+      //Fetch essential agreement parameters
       this.api.serverRequest(request_payload, "FETCH_AGREEMENT_ITEM").then(response => this.item_item_params = response);
       this.api.serverRequest(request_payload, "FETCH_AGREEMENT_TERMS").then(response => this.item_terms = response[0]);
       this.api.serverRequest(request_payload, "FETCH_ADDRESS_EVENTS").then(response => this.agreement_events = response);
 
+      //Fetch essential logistics parameters
       this.api.serverRequest({agreement_id: this.agreement_id, direction: this.direction}, "FETCH_LOGISTICS_PARAMETERS").then(response => {
         this.logistics_params = response;
         this.api.serverRequest({kolli_id: this.logistics_params.kolli_id}, "FETCH_SIMULATION_SENSORS").then(data => {
+
+          //Fetch sensors and assign the data to charts
           for (let sensor of data) {
             if (sensor.sensor_type === 'ACC') {
               this.accelerometer_sensor_id = sensor.sensor_id;
@@ -176,30 +182,34 @@ export class AgreementComponent implements OnInit {
           }
         });
       });
-
     });
   };
 
+  //Accepts the delivery
   acceptItem = function () {
     this.api.serverRequest({agreement_id: this.agreement_id, state: "COMPLETED"}, "ALTER_STATE").then();
     this.router.navigate(['itemmanager', 'bought']);
   };
 
+  //Rejects the delivery
   rejectItem = function () {
     this.api.serverRequest({agreement_id: this.agreement_id, state: "REJECTED"}, "ALTER_STATE").then();
     this.router.navigate(['itemmanager', 'bought']);
   };
 
+  //Accepts the return
   acceptReturn = function () {
     this.api.serverRequest({agreement_id: this.agreement_id, state: "INACTIVE"}, "ALTER_STATE").then();
     this.router.navigate(['itemmanager', 'bought']);
   };
 
+  //Rejects the return
   rejectReturn = function () {
     this.api.serverRequest({agreement_id: this.agreement_id, state: "CLERK"}, "ALTER_STATE").then();
     this.router.navigate(['itemmanager', 'bought']);
   };
 
+  //Redirects to ExplorerComponent with a given address as a parameter
   explore_address = function (address) {
     this.router.navigate(['explorer', address]);
   }
